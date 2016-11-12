6b8fb7b4373985d7f9f7a8e226cf0b2ae7fb47b0
Olivier Penhoat <openhoat@gmail.com>
2016-11-12 17:49:04 +0100

    initial test

--- test/servedown-test.js
+++ test/servedown-test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require('path');
 const chai = require('chai');
 const expect = chai.expect;
 const logger = require('hw-logger');
@@ -17,13 +18,21 @@ describe('servedown', () => {
   it('should instantiate a ServerDown instance', () => {
     serverdown = new ServerDown();
     expect(serverdown).to.be.ok;
+    expect(serverdown).to.have.property('locals');
     expect(serverdown).not.to.have.property('initialized');
   });
 
   it('should init', () => {
-    serverdown.init();
+    const config = {workingDir: path.join(__dirname, '..', '.working')};
+    serverdown.init(config);
     expect(serverdown).to.have.property('initialized', true);
     log.debug('serverdown :', serverdown);
+    expect(serverdown).to.have.property('config');
+    expect(serverdown.config).to.eql(config);
+    expect(serverdown).to.have.property('locals');
+    const locals = serverdown.locals;
+    expect(locals).to.have.property('config');
+    expect(locals.config).to.have.property('workingDir', config.workingDir);
   });
 
 });
