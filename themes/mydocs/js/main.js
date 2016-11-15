614d7bd989fe8c1a288eceb16773dee98f876704
Olivier Penhoat <openhoat@gmail.com>
2016-11-15 19:45:00 +0100

    search feature

--- themes/mydocs/js/main.js
+++ themes/mydocs/js/main.js
@@ -15,4 +15,27 @@ $(function() {
   $('table').addClass('table');
   $('#doc-menu > li > a').addClass('scrollto');
 
+  const searchResults = $('.highlight');
+  var searchIndex = 0;
+  if (searchResults.length) {
+    const animateToSearch = function() {
+      const searchResult = $(searchResults.get(searchIndex));
+      $('html, body').animate({
+        scrollTop: searchResult.offset().top
+      }, 1000);
+    };
+    animateToSearch();
+    $('body').keypress(function(event) {
+      if (event.which == 110 /* n key */) {
+        event.preventDefault();
+        searchIndex = (searchIndex + 1) % searchResults.length;
+        animateToSearch();
+      } else if (event.which == 112 /* p key */) {
+        event.preventDefault();
+        searchIndex = (searchIndex - 1) % searchResults.length;
+        animateToSearch();
+      }
+    });
+  }
+
 });
