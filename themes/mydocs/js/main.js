af8630e384469275881be6fdf62deff7c9606143
Olivier Penhoat <openhoat@gmail.com>
2016-11-15 15:25:12 +0100

    js refactoring

--- themes/mydocs/js/main.js
+++ themes/mydocs/js/main.js
@@ -1,5 +1,17 @@
 $(function() {
 
+  $(window).on('load resize', function() {
+    $(window).trigger('scroll');
+  });
+
+  $('body').scrollspy({target: '#doc-nav', offset: 100});
+
+  $('a.scrollto').on('click', function(e) {
+    const target = this.hash;
+    e.preventDefault();
+    $('body').scrollTo(target, 800, {offset: 0, 'axis': 'y'});
+  });
+
   $('table').addClass('table');
   $('#doc-menu > li > a').addClass('scrollto');
 
